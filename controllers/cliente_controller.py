2a1b59a003077cd0c3f300ecf0d7dfd66c5e2f7e
PauloRafael <paulorafaelbrandao@gmail.com>
2025-11-23 18:55:27 -0300

    nomes de funções atualizados

--- controllers/cliente_controller.py
+++ controllers/cliente_controller.py
@@ -8,15 +8,18 @@ from models.schema import cliente_schema
 router = APIRouter(prefix="/clientes", tags=["Clientes"])
 
 @router.post("/", response_model=cliente_schema.ClienteSchema, status_code=status.HTTP_201_CREATED)
-async def create_new_cliente(
+async def criar_novo_cliente( 
  cliente: cliente_schema.ClienteCreate, db: AsyncSession = Depends(get_db)
 ):
  return await cliente_service.create_cliente(db, cliente)
+
 @router.get("/{cliente_id}", response_model=cliente_schema.ClienteSchema)
-async def read_cliente(cliente_id: int, db: AsyncSession = Depends(get_db)):
+async def ler_cliente(
+ cliente_id: int, db: AsyncSession = Depends(get_db)
+):
     db_cliente = await cliente_service.get_cliente(db, cliente_id)
     if db_cliente is None:
-       raise HTTPException(
+        raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
         detail="Cliente não encontrado"
     )
